50d960eb5d1f443e054d2ea2ea090e41d7ee1839
Hasan Qasim <hasanqasim92@gmail.com>
2020-08-19 23:44:17 +1000

    add a network service singleton to talk to the node server

---
+++ Todo/Services/NetworkService.swift
@@ -0,0 +1,24 @@
+//
+//  NetworkService.swift
+//  Todo
+//
+//  Created by Hasan Qasim on 19/8/20.
+//  Copyright © 2020 Hasan Qasim. All rights reserved.
+//
+
+import Foundation
+
+class NetworkService {
+    static let shared = NetworkService()
+    
+    let URL_BASE = "http://localhost:3003"
+    let URL_ADD_TODO = "/add"
+    
+    func getTodos() {
+        
+    }
+    
+    func addTodo(todo: Todo) {
+        
+    }
+}
