7775504b48e48c91ba1aebbd7fa8f392aeb15462
Hasan Qasim <hasanqasim92@gmail.com>
2020-08-20 11:12:12 +1000

    add URLSession, make a dataTask with a completion handler

--- Todo/Services/NetworkService.swift
+++ Todo/Services/NetworkService.swift
@@ -14,8 +14,17 @@ class NetworkService {
     let URL_BASE = "http://localhost:3003"
     let URL_ADD_TODO = "/add"
     
+    let session = URLSession(configuration: .default)
+    
     func getTodos() {
+        let url = URL(string: URL_BASE)!
         
+        let task = session.dataTask(with: url) { (data, response, error) in
+            // debug print adds additional information suitable for debugging
+            debugPrint(data)
+        }
+        
+        task.resume()
     }
     
     func addTodo(todo: Todo) {
